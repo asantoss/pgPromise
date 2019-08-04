f383e95df4a5375c6edc1a463a7eed7939f09898
Alexander Santos <alexsantosantana@live.com>
2019-08-03 22:46:11 -0400

    Prompts are working more testing coming

--- create_album.js
+++ create_album.js
@@ -12,20 +12,17 @@ const pgp = require('pg-promise')();
 const db = pgp(config);
 const createAlbum = async function () {
     const title = await prompt('Album Title: ');
-    const release_date = await prompt('Release date (YYYY-MM-DD): ');
-    const genre = await prompt('What is the genre?: ');
-    const artist = await prompt('What is the name of the artist?: ');
+    const artist = await prompt('What is the name of the artist?: ')
+    const query = "SELECT id FROM artists WHERE name ilike $1";
     try {
-        var artist_Id = await db.one(query, artist);
+        var artist_Id = await db.one(query, artist).catch(e => console.log(e));
     } catch{
         console.error(`Artist doesn't exist, creating artist ${artist}`);
         artist_Id = await createArtist();
-        // let newArtists = `INSERT INTO artists(name) VALUES ($1)`;
-        // await db.result(newArtists, artist).then(result => console.log(`Inserted ${result.rowCount} into artists.`));
-        // var artist_Id = await db.one(query, artist).catch(e => console.error(e));
     }
+    const release_date = await prompt('Release date (YYYY-MM-DD): ');
+    const genre = await prompt('What is the genre?: ');
     const length = await prompt('What is the length of the album?: ')
-    const query = "SELECT id FROM artists WHERE name ILIKE '%$1%'";
     if (typeof artist_Id === 'object') {
         artist_Id = parseInt(artist_Id.id)
     }
@@ -47,15 +44,16 @@ const createAlbum = async function () {
 const createArtist = async function () {
     const name = await prompt('What is the name of the artist?: ');
     const query = "SELECT * FROM artists WHERE name iLike '%$1%'";
-    var artist;
+    var artist = {}
     var exists;
     await db.one(query, name).then(result => { exists = true; artist = result }).catch(() => { exists = false; console.log(`Artist doesn't appear to be in our db lets create it`) })
     if (exists) {
         console.log(`The artist exists here is the info: \n ${artist.name} \n id: ${artist.id}`)
     } else {
-        const insertArtist = "INSERT INTO artists(name) VALUES($1)"
-        await db.result(insertArtist, name).then(result => {
-            console.log(`Inserted ${result.rowCount} record into artists`)
+        const insertArtist = "INSERT INTO artists(name) VALUES($1) RETURNING id"
+        await db.one(insertArtist, name).then(result => {
+            artist.id = result.id;
+            console.log(`Inserted ${name} into artists`)
         })
     }
     prompt.finish()
@@ -63,30 +61,35 @@ const createArtist = async function () {
 }
 
 const createTrack = async function () {
-    const trackName = await prompt('What is the name of the track?: ');
-    const albumName = await prompt('What album is it in?: ');
-    const length = await prompt('What is the track duration?: ');
-    const query = "SELECT id FROM albums WHERE title ILIKE '%$1%'";
-    var albumId;
-    var albumExists;
-    await db.one(query, albumName).then(result => { albumExists = true; albumId = result.id; }).catch((e) => { albumExists = false; console.log(`Album does not exists lets create it`) })
+    const track = {}
+    track.title = await prompt('What is the name of the track?: ');
+    track.albumName = await prompt('What album is it in?: ');
+    try {
+        const query = "SELECT id FROM albums WHERE title ILIKE ${albumName}";
+        const album = await db.one(query, track).catch(e => console.log(e))
+        var albumExists = true;
+        track.albumId = album.id;
+    } catch{
+        albumExists = false
+    }
     if (albumExists) {
+        track.duration = await prompt('What is the track duration?: ');
         var inSongs;
-        var songId;
-        let querySongs = "SELECT id FROM songs WHERE title ILIKE '%$1%'"
-        await db.one(querySongs, trackName).then(results => { inSongs = true; songId = results.id; }).catch(e => { inSongs = false; console.log(trackName + ' Is not in my songs table lets add it.') })
+        var song;
+        try {
+            const querySongs = "SELECT id FROM songs WHERE title ilike '%$1%' RETURNING id"
+            inSongs = true
+            song = await db.one(querySongs, track.title)
+        } catch{
+            inSongs = false;
+        }
         if (!inSongs) {
             const insertSong = "INSERT INTO songs(title) VALUES($1) RETURNING id"
-            await db.one(insertSong, trackName).then(results => { songId = results.id; console.log(`${results.id}`) })
+            song = await db.one(insertSong, track.title)
         }
+        track.songId = song.id
         const inserTrack = "INSERT INTO tracks(title,duration,album_id,song_id) VALUES(${title},${duration},${albumId},${songId})"
-        const track = {
-            'title': trackName,
-            'duration': length,
-            'albumId': albumId,
-            'songId': songId
-        };
-        await db.result(inserTrack, track).then(results => { console.log(`Inserted ${trackName} into tracks`) }).catch(e => console.log(`${e}\n I was not able to insert this track`))
+        await db.result(inserTrack, track).catch(e => console.log(`${e}\n I was not able to insert this track`))
     } else {
         await prompt.finish()
         return createAlbum().then(() => createTrack())
@@ -133,7 +136,7 @@ const makeAlbums = function (argument) {
                 })
             })
             break;
-        default: prompt('What will you like to do?').then(val => { makeAlbums(val) })
+        default: prompt('What will you like to do? \n').then(val => { makeAlbums(val) })
     }
     return
 }
